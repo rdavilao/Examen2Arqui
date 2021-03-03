a9e26909ce5fbb059f362f431ccf1e4787f02d30
rdavilao <rdavila@espe.edu.ec>
2021-03-03 17:05:31 -0500

    Creation of Proyect2Relacional Examen

---
+++ AvilaExamenRelacional/src/main/java/ec/edu/espe/examen/relacional/RelacionalApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen.relacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacionalApplication.class, args);
+	}
+
+}
